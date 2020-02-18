e269a79ffd476a94a9e9074d65d0af6d0b82b1f8
Tök.Zsuzsa.Kinga <Tk.Zsuzsa.Kinga@C41-07>
2020-02-18 13:45:01 +0100

    Sorsoló és kiíró

--- Biliárd2.0/Program.cs
+++ Biliárd2.0/Program.cs
@@ -13,14 +13,23 @@ namespace Biliárd2._0
             List<int> golyok = new List<int>();
             Feltolto(golyok);
             int leesett=LeesoSorsolo(golyok);
-            Console.WriteLine(leesett);
+            Kiíró(golyok);
             Console.ReadKey();
+        }
+
+        static void Kiíró(List<int> golyok)
+        {
+            foreach (int golyo in golyok)
+            {
+                Console.Write($"{golyo} ");
+            }
         }
 
         static int LeesoSorsolo(List<int> golyok)
         {
             Random rnd = new Random();
             int sorsolt = rnd.Next(golyok.Count);
+            golyok.Remove(sorsolt);
             return golyok[sorsolt];
         }
 
